ce390a68a06b0c18c000192af34aa4c75f897f0e
vijeth <vijeth.7mar@gmail.com>
2018-09-17 10:18:38 +0530

    A sample Spring-Boot security application

--- src/main/java/com/vijeth/spring/config/Application.java
+++ src/main/java/com/vijeth/spring/config/Application.java
@@ -2,10 +2,9 @@ package com.vijeth.spring.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.vijeth.spring")
-public class Application extends SpringBootServletInitializer{
+public class Application{
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
